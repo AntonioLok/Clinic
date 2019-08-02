ee12d6650e4076c480cbe058ad21fee46c6755bc
Antonio Lok Chan <antoniochan030@gmail.com>
2019-08-01 22:57:35 -0400

    improve about us UI component

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -2,6 +2,39 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { browserHistory } from 'react-router';
 
+const awards = [
+  {
+    url: 'https://www.petalmd.com/hs-fs/hubfs/blog-138-digitaltransformationawards2019.png?width=900&height=450&name=blog-138-digitaltransformationawards2019.png',
+    title: '2019 Digital Transformation Awards',
+    description: 'Finalist: Artificial Intelligence Disruptor',
+  },
+  {
+    url: 'https://www.petalmd.com/hs-fs/hubfs/blog133-adriq-NL-EN.jpg?width=1300&height=650&name=blog133-adriq-NL-EN.jpg',
+    title: '2019 ADRIQ Innovation Awards Gala',
+    description: 'Finalist: Innovation | SME Award (less than 500 employees)',
+  },
+  {
+    url: 'https://www.petalmd.com/hs-fs/hubfs/event/fideides2019-winner-EN.jpg?width=1200&height=600&name=fideides2019-winner-EN.jpg',
+    title: '2018 ADRIQ Innovation Awards Gala',
+    description: 'Finalist: Innovation | SME Award (less than 500 employees)',
+  },
+  {
+    url: 'https://www.petalmd.com/hs-fs/hubfs/_PetalMD_Sept2018/Images/petalmd-about-channelinnovationawards2018.png?width=600&height=300&name=petalmd-about-channelinnovationawards2018.png',
+    title: '2018 Channel Innovation Awards',
+    description: 'Winner: Best Healthcare Solution',
+  },
+  {
+    url: 'https://www.petalmd.com/hs-fs/hubfs/_PetalMD_Sept2018/Images/petalmd-about-medlysawards2018.png?width=600&height=300&name=petalmd-about-medlysawards2018.png',
+    title: '2018 Digital Transformation Awards',
+    description: 'Finalist: Artificial Intelligence Disruptorr',
+  },
+  {
+    url: 'https://www.petalmd.com/hs-fs/hubfs/_PetalMD_Sept2018/Images/petalmd-about-healthinnovationawards.png?width=600&height=300&name=petalmd-about-healthinnovationawards.png',
+    title: '2018 TELUS Health Innovation Gala',
+    description: 'Winner: Optimizing Innovation Technology Award',
+  }
+]
+
 class AboutUs extends Component {
 
   reroute = (path) => {
@@ -9,6 +42,18 @@ class AboutUs extends Component {
   }
 
   render() {
+
+    const renderAwardRecognition = (url, title, description) => (
+        <div className="single-award-container">
+          <img alt="" src={url} />
+          <div className="award-description">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href="./about-us">Learn More</a>
+          </div>
+        </div>
+    )
+
     return (
       <div className="about-us">
         <Grid
@@ -16,96 +61,53 @@ class AboutUs extends Component {
           direction="row"
           justify="space-evenly"
           spacing={12}
+          className="banner-container"
         >
-          <Grid item xs={12} className="page-banner"
+          <Grid md={12} sm={12} xs={12} className="top-banner"
             container
             alignItems="center"
             justify="center">
-              ABOUT US
+              <div className="text-container">
+                <h1>ABOUT OUR CLINIC</h1>
+                <p>In collaboration with physicians and healthcare facility managers, PetalMD designs innovative cloud-based productivity tools and mobile applications that improve healthcare.</p>
+              </div>
           </Grid>
-          <div className="container">
-            <Grid
-              container
-              direction="row"
-              justify="space-evenly"
-              spacing={12}
-              className="why-best-section" 
-            >
-              <Grid item xs={12} md={6}
-                container
-                justify="center"
-                className="intro"
-              >
-                <div className="intro-left">
-                  <img alt="about-us" src="http://theembazaar.com/demo/themesfolios/medical-care/assets/images/image5.jpg" />
-                </div>
-              </Grid>
-              <Grid item xs={12} md={6}
-                container
-                justify="center"
-                className="intro"
-              >
-                <div className="intro-right">
-                  <h2>BEST MEDICAL & HEALTH CARE NEEDS TO OUR PATIENTS</h2>
-                  {
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat rutrum eros amet sollicitudin interdum. Suspendisse pulvinar, velit nec pharetra interdum, ante tellus ornare mi, et mollis tellus neque vitae elit. Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                    +
-                    "\n\nDonec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
-                    +
-                    "\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat rutrum eros amet sollicitudin interdum. Suspendisse pulvinar, velit nec pharetra interdum, ante tellus ornare mi, et mollis tellus neque vitae elit. Mauris adipiscing mauris fringilla turpis interdum sed pulvinar nisi malesuada."
-                  }
-                </div>
-              </Grid>
-            </Grid>
-          </div>
-          <Grid
+        </Grid>
+        <Grid
+          container
+          spacing={12}
+          className="custom-widget"
+        >
+          <Grid md={5} sm={5} xs={12} className="left-text"
             container
-            direction="row"
-            justify="space-evenly"
-            spacing={12}
-            className="why-best-section" 
-          >
-            <Grid item xs={12} md={6} className="description">
-              <div className="container">
-                <div className="title">Why we are the best</div>
-              </div>
-              <div className="icon">
-                <i className="material-icons md-60 md-light">thumb_up</i>
-                <div className="description-icon-text">
-                  <div className="title">WELL EXPERIENCED DOCTORS</div>
-                  Cras dapibus Vivamus elementum semper nisi Aenean vulputate eleifend tellus Aenean.
-                </div>
-              </div>
-              <div className="icon">
-                <i className="material-icons md-60 md-light">assignment</i>
-                <div className="description-icon-text">
-                  <div className="title">FREE MEDICAL COUNSELING</div>
-                  Cras dapibus Vivamus elementum semper nisi Aenean vulputate eleifend tellus Aenean.
-                </div>
-              </div>
-              <div className="icon">
-                <i className="material-icons md-60 md-light">wifi_tethering</i>
-                <div className="description-icon-text">
-                  <div className="title">ONLINE BILL PAYMENT</div>
-                  Cras dapibus Vivamus elementum semper nisi Aenean vulputate eleifend tellus Aenean.
-                </div>
-              </div>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <div className="img">
-              </div>
-            </Grid>
-            <Grid item xs={12}
-              className="meet-our-specialist"
-              container
-              justify="center"
-            >
-            <div
-              className="label"
-              onClick = {this.reroute.bind(this, "/doctors")}>
-              Meet our specialists
+            alignItems="center"
+            justify="center">
+              <h2>Empowering Organizations to Provide the Best Care for Their Patients</h2>
+              <p>Through actively listening to the needs of healthcare stakeholders, PetalMD has designed tools for physician scheduling, patient booking, on-call management, medical billing and secure messaging.</p>
+              <p>Combined with actionable data, our solutions optimize hospital and medical clinic performance, improve access to healthcare and save valuable time for different stakeholders.</p>
+          </Grid>
+          <Grid md={7} sm={7} xs={12} className="widget-img"
+            container
+            alignItems="center"
+            justify="center">
+              <img alt="" src="https://www.petalmd.com/hubfs/_PetalMD_Sept2018/Images/petalbooking-tour/about-empoweringEN.jpg" />
+          </Grid>
+        </Grid> 
+        <Grid
+          container
+          direction="row"
+          justify="space-evenly"
+          spacing={12}
+          className="awards-recognition"
+        >
+          <Grid md={6} sm={6} xs={12}
+            container
+            alignItems="center"
+            justify="center">
+            <div className="title">
+              Awards and Recognition
             </div>
-            </Grid>
+            {awards.map(award => renderAwardRecognition(award.url, award.title, award.description))}
           </Grid>
         </Grid>
       </div>
