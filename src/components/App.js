acfcb385ed4c9f6133610c026a82cbf6d4f42f28
USER <user@USERs-MacBook-Pro.local>
2020-02-05 15:18:57 -0500

    version 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,21 @@
-import React, {Fragment, Component } from 'react';
-import {Router , Route , browserHistory} from "react-router";
-import NavMenu from './nav-menu';  
-import Home from './home';
-import Doctors from './doctors'
-import Careers from './careers';
-import Footer from './footer';
-import AboutUs from './about-us';
-import Contact from './Contact';
+import React, { Component } from 'react';
+import { Router } from 'react-router';
+import { Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import Home from './Home';
 import '../scss/app.scss';
-import Department from './Department';
+
+const history = createHistory();
+
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <div className="app-container">
-          <NavMenu/>
-          <Router history = {browserHistory}>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/doctors" component={Doctors} />
-            <Route exact path="/about-us" component={AboutUs} />
-            <Route exact path="/careers" component={Careers} />
-            <Route exact path="/contact-us" component={Contact} />
-            <Route exact path="/departments" component={Department} />
-          </Router>
-          <Footer />
-        </div>
-      </Fragment>
+      <Router history={history}>
+      <div className="app-container">
+          <Route exact path="/" component={Home} />
+      </div>
+      </Router>
     );
   }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,73 +1,215 @@
 import React, { Component, Fragment } from 'react';
-import Slideshow from './Slideshow';
-import Hours from './Hours';
-import Colorsquare from './Colorsquare';
-import Testimonial from './Testimonial';
-import Card from './Card'; 
-import Grid from '@material-ui/core/Grid';
+import cards from './Cardarray'
 
 class Home extends Component {
+  state = {
+    dealerHand: [],
+    dealerStay: false,
+    hand: [],
+    stay: false,
+    deck: [],   //make this a representation of the deck 
+    status: ""
+  }
+
+  componentWillMount = () => {
+      this.resetDeck();
+  }
+
+  resetDeck = () => {
+    var count = 0;
+    var deck = [];
+    //deck is represented by indexes 0-51
+    while (count < 52) {
+      deck.push(count)
+      count++;
+    }
+    //deal cards to players
+    var newcardindex = (Math.floor((Math.random() * deck.length) + 1))-1;
+    var newcard1 = cards[deck[40]]
+  //  deck.splice(newcardindex,1);
+    var newcardindex2 = (Math.floor((Math.random() * deck.length) + 1))-1;
+    var newcard2 = cards[deck[41]]
+  //  deck.splice(newcardindex2,1);
+    var newcardindex3 = (Math.floor((Math.random() * deck.length) + 1))-1;
+    var newcard3 = cards[deck[50]]
+  //  deck.splice(newcardindex3,1);
+    var newcardindex4 = (Math.floor((Math.random() * deck.length) + 1))-1;
+    var newcard4 = cards[deck[5]]
+  //  deck.splice(newcardindex4,1);
+    this.setState({
+      deck: deck,
+      hand: [newcard1, newcard2],
+      dealerHand: [newcard3, newcard4],
+      status: "",
+      stay:false,
+    })
+  }
+
+  getValue = (hand) => {
+    //go through deck adding value and counting aces at the same time. count aces as 11 originally. while we are over 21 and while
+    //There are still aces
+    var count = 0;
+    var aces = 0;
+    var value = 0;
+    //Get the sum of the hand
+    while (count < hand.length) {
+      value += hand[count][1];
+      if (hand[count][0].charAt(7) === 'A') {
+        aces++;
+      }
+      count++;
+    }
+    //If we are over, see if we can get under by treating the aces as one until we are under 21
+    while (value > 21 && aces > 0) {
+      value-=10;
+      aces--;
+    } 
+    return value;
+  }
+
+
+
+  hit = () => {
+    var newcardindex = (Math.floor((Math.random() * (this.state.deck.length)) + 1))-1;
+    var newcard = cards[this.state.deck[newcardindex]]
+    var deck = this.state.deck
+    var value = this.getValue(this.state.hand)
+    var dealerValue = this.getValue(this.state.dealerHand)
+    deck.splice(newcardindex,1);
+    if (dealerValue <= 15 && (value + newcard[1]) <= 21) { //Dealer hits
+      var newcardindex1 = (Math.floor((Math.random() * (deck.length)) + 1))-1;  //we use deck here coz its adjusted
+      var newcard1 = cards[deck[newcardindex1]]
+      this.setState({
+        hand : [...this.state.hand, newcard],
+        deck: deck,
+        dealerHand: [...this.state.dealerHand, newcard1],
+      } ,() => {this.checkStatus()})
+    }
+    else { 
+    //Dealer stays and we check the status
+    this.setState({
+      hand : [...this.state.hand, newcard],
+      deck: deck,
+    }, () => {this.checkStatus()})
+    }
+  }
+
+  stay = () => {
+    //If we stay dealer hits until he has a good hand or busts
+    this.setState({
+      stay:true
+    }, () => {this.dealerHit(true)})
+  }
+
+  dealerHit = (continuous) => {
+    var newvalue = 0;
+    var newcards = [];
+    var deck = this.state.deck
+    var value = this.getValue(this.state.hand)
+    var dealerValue = this.getValue(this.state.dealerHand)
+    if (dealerValue + newvalue <=15 && dealerValue + newvalue < value) {
+      do {
+        var newcardindex = (Math.floor((Math.random() * (deck.length)) + 1))-1;
+        deck.splice(newcardindex,1);
+        newcards.push(cards[this.state.deck[newcardindex]])
+        newvalue += cards[this.state.deck[newcardindex]][1]
+      }
+      while (dealerValue + newvalue <=15 && dealerValue + newvalue < value && continuous)
+    }
+    this.setState({
+      dealerHand : [...this.state.dealerHand, ...newcards],
+      deck: deck,
+    }, () => {this.checkStatus()})
+  }
+
+  checkStatus = () => {
+    var status = "";
+    var value = this.getValue(this.state.hand)
+    var dealerValue = this.getValue(this.state.dealerHand)
+    if (value > 21) {
+      status = "L"
+      alert("You lost. You busted")
+    }
+    else if (dealerValue > 21) {
+      status  = "W"
+      alert("You won. The dealer busted")
+    }
+    else if (dealerValue > value && this.state.stay) {
+      alert("You lost. The dealer had a higher hand")
+      status  = "L"
+    }
+    else if (dealerValue < value) {
+      if (dealerValue <= 15) {
+        this.dealerHit(false)
+      }
+      else if (this.state.stay) {  //if the dealer doesnt draw and we are staying and we reveal then we won
+        alert("You won. You had a higher hand")
+        status  = "W"
+      }
+    }
+    else if (dealerValue === value) {
+      if (this.state.stay) {
+        alert("Tie")
+        status  = "T"
+      }
+    }
+    else {
+    }
+
+    this.setState ({
+      status: status
+    })
+
+  }
+
   render() {
+    var hand = [];
+    var count = 0;
+    while (count < this.state.hand.length) {
+      hand.push(<img alt = "" className = "card" src = {this.state.hand[count][0]}/>)
+      count ++;
+    }
+    count = 0
+    var dealerHand = [];
+    while (count < this.state.dealerHand.length) {
+      if (count === 0 && this.state.status.length < 1) {
+        dealerHand.push(<img alt = "" className = "card" src = "../img/red_back.png"/>)
+      }
+      else {
+        dealerHand.push(<img alt = "" className = "card" src = {this.state.dealerHand[count][0]}/>)
+      }
+      count ++;
+    }
+    var controls = [];
+    if (this.state.status.length < 1) {
+      controls.push(
+        <Fragment>
+          <button disabled = {this.state.stay} style = {{background: (this.state.stay ?  'grey' : 'maroon')}}  onClick = {this.hit}> Hit </button>
+          <button disabled = {this.state.stay} style = {{background: (this.state.stay ?  'grey' : 'maroon')}} onClick = {this.stay}> Stay </button>
+        </Fragment>
+      )
+    }
+    else {
+      controls.push(
+        <Fragment>
+          <button style = {{background:'maroon'}}  onClick = {this.resetDeck}> Reset Game </button>
+        </Fragment>
+      )
+    }
 
+    var displayValue = this.getValue(this.state.hand)
     return (
-      <Fragment>
-        <Slideshow/>
+        <div className = "homeContainer">
+          <div className = "dealerContainer"> 
+            {dealerHand}
+          </div>
+          <div className = "handContainer">
+            {controls}
+            <p className = "total"> {"Current: " + displayValue} </p>
+            {hand}
+          </div>
+        </div>
 
-        <h1> What we are offering</h1>
-        <div className="border-home"> </div>
-        <h4> Our ophthalmologists, optometrists and staff are committed to delivering a professional and enjoyable patient experience. </h4>
-
-        <Grid container spacing={0} align = "center" className = "colorSquareGrid">
-        <Grid item xs={12} sm = {6} md = {3}>
-            <Colorsquare title = "Physiology" color = "teal" body = "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."/>
-        </Grid>
-        <Grid item xs={12} sm = {6} md = {3}>
-            <Colorsquare title = "Cardiology" color = "purple" body = "Et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident."/>
-        </Grid>
-        <Grid item xs={12} sm = {6} md = {3}>
-            <Colorsquare title = "Dental" color = "steelblue" body = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta."/>
-        </Grid>
-        <Grid item xs={12} sm = {6} md = {3}>
-            <Colorsquare title = "Rehab" color = "darkred" body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud."/>
-        </Grid>
-        </Grid>
-
-        <Grid container spacing={0} align = "center" className = "middleGrid">
-        <Grid item xs={12} sm = {6} md = {8}>
-            <Testimonial text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo." name="- Christopher Greene (Patient)" img="../img/profile1.jpg" />
-            <hr/>
-            <Testimonial text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud." name="- Linda Smith (Patient)" img="../img/profile.jpg"/>
-            <hr/>
-            <Testimonial text = "Et iusto odio dignissimos quas molestias ducimus qui blanditiis ut enim ad minim  praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident." name="- Shannon Forrest (Patient)" img="../img/profile2.jpg"/>
-        </Grid>
-        <Grid item xs={12} sm = {6} md = {4}>
-            <Hours/>
-        </Grid>
-        </Grid>
-        <h1> Our Doctors</h1>
-        <div className="border"> </div>
-        <h5> Our ophthalmologists, optometrists and staff are committed to delivering a professional and enjoyable patient experience. </h5>
-        <Grid container spacing={0} align = "center" className = "doctorGrid">
-            <Grid item xs={12} sm = {6} md = {4}>
-                <Card position = "Cardiologist" name = "John Adams" src = "../img/doctor1.jpg"/>
-            </Grid>
-            <Grid item xs={12} sm = {6} md = {4}>
-                <Card position = "Physician" name = "Emily Baker"  src = "../img/doctor2.jpg"/>
-            </Grid>
-            <Grid item xs={12} sm = {6} md = {4}>
-               <Card position = "Dermatologist" name = "Robert Clark" src = "../img/doctor4.jpg"/>
-            </Grid>
-            <Grid item xs={12} sm = {6} md = {4}>
-                <Card position = "Neurologist" name = "Kylie Smith"  src = "../img/doctor6.jpg"/>
-            </Grid>
-            <Grid item xs={12} sm = {6} md = {4}>
-                <Card position = "Surgeon" name = "James Davis"  src = "../img/doctor3.jpg"/>
-            </Grid>
-            <Grid item xs={12} sm = {6} md = {4}>
-               <Card position = "Anesthesiologist" name = "Sarah Jones"  src = "../img/doctor5.jpg"/>
-            </Grid>
-        </Grid>
-      </Fragment>
     );
   }
 }
